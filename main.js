323fb49588afdedefa47d7f0700c2e5bd56443b6
Eron Alves <eron.alves@rocketmail.com>
2022-06-25 12:20:20 -0300

    Finalized the functionality to choose from breed

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const buttonCat = document.querySelector('#cats button');
 const buttonDog = document.querySelector('#dogs button');
-const dogSelection = document.querySelector("#selectDog");
+const dogSelection = document.querySelector("#select-dog");
 let DogList;
 
 buttonCat.addEventListener('click', changePet);
@@ -11,13 +11,20 @@ async function changePet(e) {
     const imgCat = document.querySelector('#cats .pet-img');
 
     if (e.target.className === "dog") {
-        const newDog = await getNewDog();
+        let newDog;
+        try {
+            const subBreed = document.querySelector("#subselect-dog");
+            newDog = await getNewDog(dogSelection.value, subBreed.value);
+        } catch (e) {
+            console.log("Sub-breed not found");
+            newDog = await getNewDog(dogSelection.value);
+        }
         imgDog.src = newDog.message;
     } else if (e.target.className === "cat") {
         const newCat = await getNewCat();
         imgCat.src = newCat[0].url;
     } else {
-        const newDog = await getNewDog();
+        const newDog = await getNewDog("random");
         imgDog.src = newDog.message;
         const newCat = await getNewCat();
         imgCat.src = newCat[0].url;
@@ -37,9 +44,17 @@ async function getNewCat(){
     .then(response => response.json());
 }
 
-async function getNewDog(){
-    return await fetch("https://dog.ceo/api/breeds/image/random")
-    .then(response => response.json());
+async function getNewDog(breed, subbreed = "none"){
+    if(breed === "random") return await fetch("https://dog.ceo/api/breeds/image/random")
+        .then(response => response.json());
+    else {
+        let FORMATTED_URL;
+        if(subbreed !== "none") FORMATTED_URL = `https://dog.ceo/api/breed/${breed}/${subbreed}/images/random`;
+        else FORMATTED_URL = `https://dog.ceo/api/breed/${breed}/images/random`;
+
+        return await fetch(FORMATTED_URL)
+        .then(response => response.json());
+    }
 }
 
 addEventListener("DOMContentLoaded", changePet);
@@ -70,6 +85,7 @@ dogSelection.addEventListener('change', ()=>{
     }
     if(verifyArray.length > 0) {
         const subSelect = document.createElement("select");
+        subSelect.id = "subselect-dog";
         verifyArray.forEach(n=>{
             const subOption = document.createElement("option");
             subOption.textContent = n;
